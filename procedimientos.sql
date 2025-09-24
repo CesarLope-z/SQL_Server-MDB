d1fb755a09ba2ce48c03d466011b077c7023252c
César López <90667173+CesarLope-z@users.noreply.github.com>
2025-09-24 09:49:38 -0600

    Crear procedimientos.sql

---
+++ procedimientos.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Categoria (
+    CategoriaId INT PRIMARY KEY,        -- identificador
+    CategoriaNombre VARCHAR(15) UNIQUE  -- nombre de la categoría, único
+);
+
+
+create procedure insertar_categorias
+@id int
+@nombreCategoria varchar(15)
+as 
+if(select count(*) from categoria 
+  where categoriaId=@id or categoriaNombre=@nombreCategoria
+)=0
+insert into categoria(categoriaNombre)
+values(@NombreCategoria)
+else 
+print('Error categoria ya existe')
